9f1b6a324932e6c78be4eb2395f36f83c6282f21
Cibirsk <stvl@gmx.com>
2021-04-12 22:25:45 +0200

    Update HF_JS_test.js

--- HF_JS_test.js
+++ HF_JS_test.js
@@ -1,20 +1,25 @@
-function printHighScore(arr){
+function printHighScore(arrScores,arrCost){
     let result=[];
     let hightest=0;
-    for(let i=0;i<scores.length;i++){
-        if(scores[i]>=hightest){
-            hightest=scores[i];
+    let bestValue=0;
+    let bestValueIndex=[];
+    for(let i=0;i<arrScores.length;i++){
+        if(arrScores[i]>=hightest){
+            hightest=arrScores[i];
         }
     }
 
-    for(let i=0;i<scores.length;i++){
-        if(scores[i] == hightest){
+    for(let i=0;i<arrScores.length;i++){
+        if(arrScores[i] == hightest){
             result.push(i);
+            bestValueIndex.push()
         }
 }
-    return `The numbers tests are: ${scores.length} ,
-    the hightest score is: ${hightest}
-    and the solutions: ${result}`;
+
+
+    return `The numbers tests are:  ${arrScores.length} ,
+    the hightest score is:  ${hightest}
+    and the solutions:  ${result}`;
 }
 
 let scores = [60, 50, 60, 58, 54, 54,
@@ -24,4 +29,11 @@ let scores = [60, 50, 60, 58, 54, 54,
 46, 31, 57, 52, 44, 18,
 41, 53, 55, 61, 51, 44];
 
-console.log(printHighScore(scores));
+let costs = [.25, .27, .25, .25, .25, .25,
+.33, .31, .25, .29, .27, .22,
+.31, .25, .25, .33, .21, .25,
+.25, .25, .28, .25, .24, .22,
+.20, .25, .30, .25, .24, .25,
+.25, .25, .27, .25, .26, .29];
+
+console.log(printHighScore(scores,costs));
