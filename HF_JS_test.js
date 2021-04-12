cd8e347d03ed0faed58faf5653bb125e9e55ed9a
Cibirsk <stvl@gmx.com>
2021-04-12 14:27:22 +0200

    Update HF_JS_test.js

--- HF_JS_test.js
+++ HF_JS_test.js
@@ -1,13 +1,38 @@
-function phrase(){
-    let word1=["Un", "Deux","Trois","Quatre","Cinq"];
-    let word2=["petit","grand","gros","gentil","méchant"];
-    let word3=["chat","chien","loup","lapin","cerf"];
+let scores = [60, 50, 60, 58, 54, 54,
+58, 50, 52, 54, 48, 69,
+34, 55, 51, 52, 44, 51,
+69, 64, 66, 55, 52, 61,
+46, 31, 57, 52, 44, 18,
+41, 53, 55, 61, 51, 44];
 
-    let ranWord1= word1[Math.floor(Math.random() * word1.length)];
-    let ranWord2= word2[Math.floor(Math.random() * word2.length)];
-    let ranWord3= word3[Math.floor(Math.random() * word3.length)];
-
-    return ranWord1 + " " + ranWord2 + " " + ranWord3;
+let result=[];
+let hightest=0;
+for(let i=0;i<scores.length;i++){
+    if(scores[i]>=hightest){
+        hightest=scores[i];
+    }
 }
 
-console.log(phrase());
+for(let i=0;i<scores.length;i++){
+    if(scores[i] == hightest){
+        result.push(i);
+    }
+}
+
+console.log(`numbers tests: ${scores.length}`);
+console.log(`The hightest score is: ${hightest}`);
+console.log(`solutions with the hightest: ${result}`);
+
+
+var products = ["Choo Choo Chocolate", "Icy Mint", "Cake Batter","Bubblegum"];
+var hasBubbleGum = [false,false,false,true];
+var i = 0;
+
+while (i < hasBubbleGum.length){
+    if (hasBubbleGum[i]){
+        console.log(products[i] + " contains bubble gum");
+        i = i + 1;
+    }
+}
+
+
