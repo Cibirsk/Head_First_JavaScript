4145455f0029977b70a61731a897fecd899e4e87
Cibirsk <stvl@gmx.com>
2021-04-14 16:55:35 +0200

    Update HF_JS_test.js

--- HF_JS_test.js
+++ HF_JS_test.js
@@ -1,24 +1,13 @@
-let carCaract = {
+function makeCar(){
+    let marque=["Fiat","VW","Mercedes","Audi"];
+    let passager=[2,4,6];
+    let couleur=["rouge","jaune","bleue","noire"];
+    let annee=[1969,1970,1981,2021];
+    let convertible=[true,false];
 
+    let randMarque= Math.floor(Math.random() * marque.length);
+    let randPassager= Math.floor(Math.random() * passager.length);
+    let randCouleur= Math.floor(Math.random() * couleur.length);
+    let randAnnee= Math.floor(Math.random() * annee.length);
+    let randConvertible= Math.floor(Math.random() * convertible.length);
 }
-let taxi = {
-}
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
