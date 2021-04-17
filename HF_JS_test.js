e58e90ca21e043fb5c67c30a33c5e0e0d9754a2c
Cibirsk <stvl@gmx.com>
2021-04-17 11:06:19 +0200

    none

--- HF_JS_test.js
+++ HF_JS_test.js
@@ -1,5 +1,10 @@
 function init(){
-    let myPlanet = document.getElementById("planet");
-    myPlanet.innerHTML = "The BIG test";
+    let planet1 = document.getElementById("planet1");
+    let planet2 = document.getElementById("planet2");
+    let planet3 = document.getElementById("planet3");
+    planet1.innerHTML = "Saturne";
+    planet1.setAttribute("class", "redtext");
+    planet2.innerHTML = "Terre";
+    planet3.innerHTML = "Mars";
 }
 window.onload=init;
