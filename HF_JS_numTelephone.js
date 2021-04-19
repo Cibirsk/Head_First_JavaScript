b3bc569a88acc4d10265fd48f0e2886bb4ae7c80
Cibirsk <stvl@gmx.com>
2021-04-19 20:31:42 +0200

    Update HF_JS_numTelephone.js

--- HF_JS_numTelephone.js
+++ HF_JS_numTelephone.js
@@ -14,6 +14,7 @@
     return false;
 }
 */
+/*
 function validNumber(theNumb){
     for(let i=0;i<theNumb.length;i++){
         if(theNumb.charAt(3) == "-" && theNumb.length == 8){
@@ -29,7 +30,21 @@ function validNumber(theNumb){
     return false;
 }
 
+
+
 let theNumb = prompt("give a number on the form 123-4567: ");
 
-console.log(validNumber(theNumb));
+console.log(validNumber(theNumb)); */
+
+function Duck(sound) {
+    this.sound = sound;
+    this.quack = function() {console.log(this.sound);}
+}
+
+var toy = new Duck("quack quack");
+
+toy.quack();
+
+console.log(typeof toy);
+console.log(toy instanceof Duck);
 
