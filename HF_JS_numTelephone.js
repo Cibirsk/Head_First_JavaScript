b9a62f3ef01e038b8b1a5e57eb77285f46709f0c
Cibirsk <stvl@gmx.com>
2021-04-19 17:29:41 +0200

    Update HF_JS_numTelephone.js

--- HF_JS_numTelephone.js
+++ HF_JS_numTelephone.js
@@ -1,11 +1,29 @@
-//vérifier si un numéro de téléphone est de la forme 111-1111
-function validNumber(theNumb){
+//vérifier si un numéro de téléphone est de la forme 111-1111 ou 1111111
+/*function validNumber(theNumb){
     for(let i=0;i<theNumb.length;i++){
         if(theNumb.charAt(3) == "-" && theNumb.length == 8){
             let tempStr = theNumb.slice(0,3) + theNumb.slice(4);
             if(!isNaN(tempStr)){
                 return true;
             }
+        }
+        if(theNumb.length == 7 && !isNaN(theNumb)){
+            return true;
+        }
+    }
+    return false;
+}
+*/
+function validNumber(theNumb){
+    for(let i=0;i<theNumb.length;i++){
+        if(theNumb.charAt(3) == "-" && theNumb.length == 8){
+            let tempStr = theNumb.split("-") ;
+            if(!isNaN(tempStr[i])){
+                return true;
+            }
+        }
+        if(theNumb.length == 7 && !isNaN(theNumb)){
+            return true;
         }
     }
     return false;
@@ -14,3 +32,4 @@ function validNumber(theNumb){
 let theNumb = prompt("give a number on the form 123-4567: ");
 
 console.log(validNumber(theNumb));
+
